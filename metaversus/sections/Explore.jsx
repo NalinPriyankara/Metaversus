0ab3c9db9078c1694e099f87286fefb717993289
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-07-12 21:38:51 +0530

    exploreCards

--- metaversus/sections/Explore.jsx
+++ metaversus/sections/Explore.jsx
@@ -6,9 +6,12 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { staggerContainer } from '../utils/motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
-import { exploreworlds } from '../constants';
+import { exploreWorlds } from '../constants';
 
-const Explore = () => (
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
+
+  return (
   <section className={`${styles.paddings}`} id='explore'>
     <motion.div
       variants={staggerContainer}
@@ -19,8 +22,20 @@ const Explore = () => (
     >
       <TypingText title="| The World" textStyles="text-center" />
       <TitleText title={<>Choose the world you want <br className='md:block hidden' /> to explore </>} textStyles='text-center' />
+      <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
+        {exploreWorlds.map((world, index) => (
+          <ExploreCard
+            key={world.id}
+            {...world}
+            index={index}
+            active={active}
+            handleClick={setActive}
+          />
+        ))}
+      </div>
     </motion.div>
   </section>
-);
+  )
+};
 
 export default Explore;
