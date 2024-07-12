9245dbb8d1207d17e7cf543ee56191ce755486a5
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-07-12 13:09:29 +0530

    navbar

--- metaversus/sections/About.jsx
+++ metaversus/sections/About.jsx
@@ -1,7 +1,14 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div />
     About section
   </section>
 );
